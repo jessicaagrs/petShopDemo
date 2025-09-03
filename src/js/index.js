1770ca35ac3fc763ba93464a053b454f988244f9
Jessica Aguiar <jessicaag.rs@gmail.com>
2025-09-03 13:55:44 -0300

    feat: enhance landing page with new animations and favicon
    ✔️ Adds a favicon to the project
    ✔️ Implements new animation classes for elements
    ✔️ Updates CSS styles for improved layout and responsiveness
    ✔️ Removes unused animations CSS file

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,25 @@
-function observeElement(element, className = '') {
-  if (!element) return;
+const animationsMap = {
+  slideInBl: 'slide-in-bl',
+  slideInRight: 'slide-in-right',
+  slideInBottom: 'slide-in-bottom',
+  slitInVertical: 'slit-in-vertical',
+  fadeInLeft: 'fade-in-left',
+  fadeInRight: 'fade-in-right',
+  pulsate: 'pulsate-fwd',
+};
 
-  const observer = new IntersectionObserver(entries => {
+const observer = new IntersectionObserver(
+  entries => {
     entries.forEach(entry => {
-      if (entry.isIntersecting && className) {
-        entry.target.classList.add(className);
-        return;
+      const anim = animationsMap[entry.target.dataset.animate];
+      if (anim) {
+        entry.target.classList.toggle(anim, entry.isIntersecting);
       }
 
-      if (entry.isIntersecting) {
-        element.currentTime = 0;
-        element.play();
-      }
+      if (entry.isIntersecting) observer.unobserve(entry.target);
     });
-  });
+  },
+  { threshold: 0.5 }
+);
 
-  observer.observe(element);
-}
-
-window.addEventListener('load', () => {
-  const listServices = document.querySelectorAll('#list-services li');
-
-  listServices.forEach((item, index) => {
-    if (index === 0) {
-      observeElement(item, 'fade-in-left');
-    } else {
-      setTimeout(() => {
-        observeElement(item, 'fade-in-left');
-      }, index * 300);
-    }
-  });
-
-//   const video = document.querySelector('.video video');
-//   observeElement(video);
-
-  const teamTag = document.querySelector('.team-tag');
-  observeElement(teamTag, 'slide-in-blurred-right');
-
-  const professionalItems = document.querySelectorAll('[data-professional]');
-  professionalItems.forEach(item => {
-    observeElement(item, 'puff-in-ver');
-  });
-
-  const itemsTestimonials = document.querySelectorAll('.item');
-  itemsTestimonials.forEach((item, index) => {
-    if (index % 2 === 0) {
-      observeElement(item, 'slide-in-blurred-right');
-    } else {
-      observeElement(item, 'slide-in-blurred-left');
-    }
-  });
-
-  const iconSocials = document.querySelectorAll('.footer a > img');
-  iconSocials.forEach(item => {
-    observeElement(item, 'bounce-top');
-  });
-});
+document.querySelectorAll('[data-animate]').forEach(el => observer.observe(el));
