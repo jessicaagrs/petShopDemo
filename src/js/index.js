c52d288e4e345b84e86f4c025922efcab959dc4e
Jessica Aguiar <jessicaag.rs@gmail.com>
2025-09-15 16:06:56 -0300

    refactor: Refactor CSS and JavaScript files for improved readability and performance

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,2 @@
-const animationsMap = {
-  slideInBl: 'slide-in-bl',
-  slideInRight: 'slide-in-right',
-  slideInBottom: 'slide-in-bottom',
-  slitInVertical: 'slit-in-vertical',
-  fadeInLeft: 'fade-in-left',
-  fadeInRight: 'fade-in-right',
-  pulsate: 'pulsate-fwd',
-};
-
-const observer = new IntersectionObserver(
-  entries => {
-    entries.forEach(entry => {
-      const anim = animationsMap[entry.target.dataset.animate];
-      if (anim) {
-        entry.target.classList.toggle(anim, entry.isIntersecting);
-      }
-
-      if (entry.isIntersecting) observer.unobserve(entry.target);
-    });
-  },
-  { threshold: 0.5 }
-);
-
-document.querySelectorAll('[data-animate]').forEach(el => observer.observe(el));
+const animationsMap={slideInBl:'slide-in-bl',slideInRight:'slide-in-right',slideInBottom:'slide-in-bottom',slitInVertical:'slit-in-vertical',fadeInLeft:'fade-in-left',fadeInRight:'fade-in-right',pulsate:'pulsate-fwd',};const observer=new IntersectionObserver(entries=>{entries.forEach(entry=>{const anim=animationsMap[entry.target.dataset.animate];if(anim){entry.target.classList.toggle(anim,entry.isIntersecting)}
+if(entry.isIntersecting)observer.unobserve(entry.target);})},{threshold:0.5});document.querySelectorAll('[data-animate]').forEach(el=>observer.observe(el))
